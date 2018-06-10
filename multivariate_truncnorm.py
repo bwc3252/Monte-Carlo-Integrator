78cd362a6d8b37f32db341ff440882449d8f9808
ben <bwc3252@rit.edu>
2018-06-10 17:06:56 -0400

    Made improvements to multivariate truncnorm

--- multivariate_truncnorm.py
+++ multivariate_truncnorm.py
@@ -49,10 +49,10 @@ def get_multipliers(cov):
 def get_one_sample(bounds, new_bounds, mean, q):
     d = len(bounds)
     count = 0
+    llim = new_bounds[:,[0]]
+    rlim = new_bounds[:,[1]]
     while True:
         count += 1
-        llim = new_bounds[:,[0]]
-        rlim = new_bounds[:,[1]]
         sample = np.rot90(truncnorm.rvs(llim, rlim, loc=0, scale=1))
         sample = np.dot(q, sample[0]) + mean # unrotated here
         if sample_in_region(sample, bounds):
@@ -71,19 +71,22 @@ def sample_in_region(sample, bounds):
     return True
 
 
-def sample(mean, cov, bounds, n, ratio=False):
+def sample(mean, cov, bounds, n):
     mean = np.matrix(mean)
     d = len(bounds)
     q = get_multipliers(cov)
     new_bounds = get_new_bounds(bounds - mean, q)
-    count = 0
-    total = 0.0
-    samples = np.empty((n, d))
-    while count < n:
-        samples[count], m = get_one_sample(bounds, new_bounds, mean, q)
-        total += m
-        count += 1
-    if ratio:
-        return samples, n / total
-    else:
-        return samples
+    llim_new = new_bounds[:,[0]]
+    rlim_new = new_bounds[:,[1]]
+    ret = np.empty((0, d))
+    while len(ret) < n:
+        samples = np.rot90(truncnorm.rvs(llim_new, rlim_new, loc=0, scale=1, size=(d, n)))
+        samples = np.rot90(np.inner(q, samples)) + mean
+        llim = np.rot90(bounds[:,[0]])
+        rlim = np.rot90(bounds[:,[1]])
+        replace1 = np.greater(samples, llim).all(axis=1)
+        replace2 = np.less(samples, rlim).all(axis=1)
+        replace = np.array(np.logical_and(replace1, replace2)).flatten()
+        to_append = samples[replace]
+        ret = np.append(ret, to_append, axis=0)
+    return ret[:n]
--- multivariate_truncnorm_test.py
+++ multivariate_truncnorm_test.py
@@ -5,35 +5,68 @@ from time import time
 
 import multivariate_truncnorm as truncnorm
 
-bounds = np.matrix([[-5, 5], [0, 5]])
-cov = np.matrix([[.1, .3], [.3, 1]])
+bounds = np.matrix([[0, 3], [0, 5]])
+cov = np.matrix([[1, .8], [.8, 2]])
 mean = np.array([0, 0])
-n = 5000
+n = 50000
 
 t1 = time()
-samples1, ratio = truncnorm.sample(mean, cov, bounds, n, ratio=True)
+samples1= truncnorm.sample(mean, cov, bounds, n)
 t2 = time()
 
 t3 = time()
 samples2 = np.random.multivariate_normal(mean, cov, n)
 t4 = time()
 
-print('truncnorm time:', t2 - t1, 'seconds, with acceptance ratio of', ratio)
+print('truncnorm time:', t2 - t1, 'seconds')
 print('numpy time:', t4 - t3, 'seconds')
 
 plt.figure(1)
-plt.subplot(211)
+plt.subplot(221)
 plt.axis([-5, 5, -5, 5])
-plt.scatter(samples1[:,[0]], samples1[:,[1]])
+#print( samples1.T)
+x=np.ravel((samples1.T)[0]); y=np.ravel(samples1.T[1]);
+#print(x,y)
+plt.scatter(x,y)
 
-plt.figure(1)
-plt.subplot(212)
+plt.subplot(222)
 plt.axis([-5, 5, -5, 5])
 plt.scatter(samples2[:,[0]], samples2[:,[1]])
-plt.show()
 
+plt.subplot(223)
 plt.hist(samples1[:,[0]], 50)
-plt.show()
 
+plt.subplot(224)
 plt.hist(samples1[:,[1]], 50)
-plt.show()
+#plt.show()
+plt.savefig("dat.png")
+
+
+def mycdf(dat):
+    def counter(x):
+        if isinstance(x,float):
+            return np.sum(dat < x)*1.0/len(dat)
+        else:
+            yvals = np.zeros(len(x))
+            for indx in np.arange(len(yvals)):
+                yvals[indx] = np.sum(dat < x[indx])*1.0/len(dat)
+            return yvals
+    return counter
+
+# Apply constraints on multivariate normal
+indx_ok = np.logical_and(samples2[:,0]>bounds[0,0]  , samples2[:,0]<bounds[0,1] )
+indx_ok = np.logical_and(indx_ok, samples2[:,1]>bounds[1,0])
+indx_ok = np.logical_and(indx_ok, samples2[:,1]<bounds[1,1])
+samples2 = samples2[indx_ok]
+
+# Confirm CDF is consistent
+fn = mycdf(samples2[:,0])
+xvals = np.linspace(bounds[0,0], bounds[0,1],100)
+yvals = fn(xvals)
+plt.clf()
+plt.plot(xvals,yvals)
+fn = mycdf(samples1[:,0])
+xvals = np.linspace(bounds[0,0], bounds[0,1],100)
+yvals = fn(xvals)
+plt.plot(xvals,yvals)
+plt.savefig("cdf.png")
