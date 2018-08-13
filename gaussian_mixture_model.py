Fixed variance and eff_samp calculations

gaussian_mixture_model.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from scipy.misc import logsumexp
 import multivariate_truncnorm as truncnorm
@@ -9,11 +8,6 @@ import itertools
 '''
 Equation references are from Numerical Recipes for general GMM and https://www.cs.nmsu.edu/~joemsong/publications/Song-SPIE2005-updated.pdf
 for online updating features
-
-Things to do:
-    Add weights to fit()
-    Write a score() function
-    Write a sample() function
 '''
 
 class estimator:
@@ -225,19 +219,17 @@ class gmm:
         if sample_weights is None:
             sample_weights = np.ones((n, 1))
         x_avg = [np.average(sample_array, axis=0, weights=sample_weights.flatten())]
-        weight = np.sum(sample_weights)
         for dim in range(d):
             x_copy = np.copy(x_avg)
             llim = bounds[dim][0]
             rlim = bounds[dim][1]
+            weight = np.sum(sample_weights) / (rlim - llim)
             ldiff = x_avg[0][dim] - llim
             rdiff = rlim - x_avg[0][dim]
             x_copy[0][dim] = llim - ldiff
-            print(x_copy)
             sample_array = np.append(sample_array, x_copy, axis=0)
             sample_weights = np.append(sample_weights, [[weight]], axis=0)
             x_copy[0][dim] = rlim + rdiff
-            print(x_copy)
             sample_array = np.append(sample_array, x_copy, axis=0)
             sample_weights = np.append(sample_weights, [[weight]], axis=0)
         return sample_array, sample_weights

monte_carlo_integrator.py
@@ -4,14 +4,50 @@ import gaussian_mixture_model as GMM
 
 
 class integrator:
+    '''
+    Class to iteratively perform an adaptive Monte Carlo integral where the integrand
+    is a combination of one or more Gaussian curves, in one or more dimensions.
 
-    def __init__(self, d, bounds, gmm_dict, n_comp, reflect=False):
+    Parameters:
+
+    d (int): Total number of dimensions.
+
+    bounds (2 x d numpy array): Limits of integration, where each row represents
+    [left_lim, right_lim] for its corresponding dimension.
+
+    gmm_dict (dict): Dictionary where each key is a tuple of one or more dimensions
+    that are to be modeled together. If the integrand has strong correlations between
+    two or more dimensions, they should be grouped. Each value is by default initialized
+    to None, and is replaced with the GMM object for its dimension(s).
+
+    n_comp (int): The number of Gaussian components per group of dimensions.
+
+    reflect (bool or 2 x d numpy array): Option to reflect all samples across limits of
+    integration to help handle boundaries. If not False, must be a numpy array of the
+    same shape as the bounds parameter, where each entry is a bool indicating whether
+    or not to reflect over that boundary.
+
+    trunc_corr (bool): An alternative option to correct for boundaries. Currently
+    does not work.
+
+    prior_samples (d x n numpy array): User-provided samples for intial evaluation
+    and training. If None, a uniform prior is used. Note that if non-uniform prior
+    is used, the prior_pdf must also be provided.
+
+    prior_pdf (1 x n numpy array): User-provided responsibilities for prior samples
+    '''
+
+    def __init__(self, d, bounds, gmm_dict, n_comp, reflect=False, trunc_corr=False,
+                    prior_samples=None, prior_pdf=None):
         # user-specified parameters
         self.d = d
         self.bounds = bounds
         self.gmm_dict = gmm_dict
         self.n_comp = n_comp
-        self.reflect=reflect
+        self.reflect = reflect
+        self.trunc_corr = trunc_corr
+        self.prior_samples = prior_samples
+        self.prior_pdf = prior_pdf
         # constants
         self.t = 0.02 # percent estimated error threshold
         self.n = (5000 * self.d) # number of samples per batch
@@ -22,9 +58,15 @@ class integrator:
         self.integral = 0
         self.var = 0
         self.eff_samp = 0
-        self.prev_weights_sum = 0 # for weighted averages
+        self.iterations = 0 # for weighted averages and count
+        self.max_value = float('-inf') # for calculating eff_samp
+        self.total_value = 0 # for calculating eff_samp
 
     def sample(self):
+        if self.prior_samples is not None:
+            self.sample_array, self.p_array = self.prior_samples, self.prior_pdf
+            self.prior_samples, self.prior_pdf = None, None
+            return
         self.p_array = np.ones((self.n, 1))
         self.sample_array = np.empty((self.n, self.d))
         for dim_group in self.gmm_dict: # iterate over grouped dimensions
@@ -57,15 +99,20 @@ class integrator:
     def train(self):
         if self.reflect:
             # we need to reflect each dimension over the left and right limits
-            sample_array, value_array, p_array = self.reflect_over_bounds()
-            new_n_comp = self.n_comp * (3 ** self.d) # reflection increases number of components
+            sample_array, value_array, p_array, new_n_comp = self.reflect_over_bounds()
             new_n, _ = sample_array.shape
         else:
             sample_array, value_array, p_array = self.sample_array, self.value_array, self.p_array
             new_n_comp = self.n_comp
             new_n = self.n
-        weights = value_array / p_array # training weights for samples
+        weights = abs(value_array / p_array) # training weights for samples
         for dim_group in self.gmm_dict: # iterate over grouped dimensions
+            # create a matrix of the left and right limits for this set of dimensions
+            new_bounds = np.empty((len(dim_group), 2))
+            index = 0
+            for dim in dim_group:
+                new_bounds[index] = self.bounds[dim]
+                index += 1
             model = self.gmm_dict[dim_group] # get model for this set of dimensions
             temp_samples = np.empty((new_n, len(dim_group)))
             index = 0
@@ -76,11 +123,11 @@ class integrator:
             if model is None:
                 # model doesn't exist yet
                 model = GMM.gmm(new_n_comp)
-                model.fit(temp_samples, sample_weights=weights)
+                model.fit(temp_samples, sample_weights=weights, bounds=new_bounds, trunc_corr=self.trunc_corr)
             else:
-                model.update(temp_samples, sample_weights=weights)
+                model.update(temp_samples, sample_weights=weights, bounds=new_bounds, trunc_corr=self.trunc_corr)
             self.gmm_dict[dim_group] = model
-            #model.print_params()
+
 
     def reflect_over_bounds(self):
         # make local copies of points, function values, and responsibilities
@@ -90,56 +137,65 @@ class integrator:
         # get rotated arrays of function values and responsibilities
         rotated_values = np.flipud(value_array)
         rotated_p = np.flipud(p_array)
+        new_n_comp = self.n_comp
         for dim in range(self.d): # each dimension will have two reflections
-            # get reflections of points in this dimension
-            right = sample_array
-            right[:,[dim]] *= -1
-            left = right
-            # get limits to reflect over
-            llim = self.bounds[dim][0]
-            rlim = self.bounds[dim][1]
-            # x_right = (rlim - x) + rlim = (-1 * x) + (2 * rlim)
-            # x_left = llim - (x - llim) = (-1 * x) + (2 * llim)
-            right[:,[dim]] += 2 * rlim
-            left[:,[dim]] += 2 * llim
-            # append new values to all arrays
-            sample_array = np.append(sample_array, right, axis=0)
-            sample_array = np.append(sample_array, left, axis=0)
-            value_array = np.append(value_array, value_array, axis=0)
-            value_array = np.append(value_array, rotated_values, axis=0)
-            p_array = np.append(p_array, p_array, axis=0)
-            p_array = np.append(p_array, rotated_p, axis=0)
-        return sample_array, value_array, p_array
+            if self.reflect[dim][1]: # right side needs reflected
+                # get a copy of samples
+                right = np.copy(sample_array)
+                # reflect
+                right[:,[dim]] *= -1
+                # shift
+                rlim = self.bounds[dim][0]
+                right[:,[dim]] += 2 * rlim
+                # append things
+                sample_array = np.append(sample_array, right, axis=0)
+                value_array = np.append(value_array, value_array, axis=0)
+                p_array = np.append(p_array, p_array, axis=0)
+                new_n_comp += self.n_comp
+            if self.reflect[dim][0]: # left side needs reflected
+                # get a copy of samples
+                left = np.copy(sample_array)
+                # reflect
+                left[:,[dim]] *= -1
+                # shift
+                llim = self.bounds[dim][0]
+                left[:,[dim]] += 2 * llim
+                # append things
+                sample_array = np.append(sample_array, left, axis=0)
+                value_array = np.append(value_array, rotated_values, axis=0)
+                p_array = np.append(p_array, rotated_p, axis=0)
+                new_n_comp += self.n_comp
+        return sample_array, value_array, p_array, new_n_comp
 
     def calculate_results(self):
         # make local copies
-        value_array = self.value_array
-        p_array = self.p_array
-        # get weight of current iteration
-        weight = np.sum(p_array)
-        # calculate variance
-        curr_var = np.var(value_array)
-        # calculate eff_samp
-        curr_eff_samp = np.sum(value_array) / np.max(value_array)
-        # calculate integral
+        value_array = np.copy(self.value_array)
+        p_array = np.copy(self.p_array)
         value_array /= p_array
+        # get weight of current iteration
+        weight = np.sum(value_array)
+        # calculate variance
+        curr_var = np.var(value_array) / self.n
+        # calculate eff_samp
+        max_value = np.max(value_array)
+        if max_value > self.max_value:
+            self.max_value = max_value
+        self.total_value += np.sum(value_array)
+        self.eff_samp = self.total_value / self.max_value
+        # calculate integral
         curr_integral = (1.0 / self.n) * np.sum(value_array)
         # update results
-        self.integral = ((self.integral * self.prev_weights_sum) + (curr_integral * weight)) / (self.prev_weights_sum + weight)
-        self.var = ((self.var * self.prev_weights_sum) + (curr_var * weight)) / (self.prev_weights_sum + weight)
-        self.eff_samp = ((self.eff_samp * self.prev_weights_sum) + (curr_eff_samp * weight)) / (self.prev_weights_sum + weight)
-        # update weight sum
-        self.prev_weights_sum += weight
+        self.integral = ((self.integral * self.iterations) + curr_integral) / (self.iterations + 1)
+        self.var = ((self.var * self.iterations) + curr_var) / (self.iterations + 1)
 
 
     def integrate(self, func, min_iter=10, max_iter=20, var_thresh=0.03):
-        count = 0
-        while count < max_iter:
+        while self.iterations < max_iter:
             self.sample()
             self.value_array = func(self.sample_array)
-            self.train()
             self.calculate_results()
             print(self.integral, '+/-', np.sqrt(self.var), 'with eff_samp', self.eff_samp)
-            count += 1
-            if count >= min_iter and self.var < var_thresh:
+            self.iterations += 1
+            if self.iterations >= min_iter and self.var < var_thresh:
                 break
+            self.train()